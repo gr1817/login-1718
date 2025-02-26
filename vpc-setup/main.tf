23fda9fcab7853f2e46cd5334ec34cd3e5635497
gr1817 <gopirajuyadala2@gmail.com>
2025-02-27 01:55:47 +0530

    add private subnet

--- vpc-setup/main.tf
+++ vpc-setup/main.tf
@@ -34,3 +34,16 @@ resource "aws_subnet" "login-be-subnet" {
     Name = "login-backend-subnet"
   }
 }
+
+#Subnet for database
+
+resource "aws_subnet" "login-db-subnet" {
+  vpc_id     = aws_vpc.login-vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = "false"
+
+  tags = {
+    Name = "login-database-subnet"
+  }
+}
