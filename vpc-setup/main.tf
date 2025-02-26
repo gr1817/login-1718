13e1e397ca6146169626f61274e3d8309a874ae2
gr1817 <gopirajuyadala2@gmail.com>
2025-02-27 01:40:59 +0530

    creating subnet for frontend

--- vpc-setup/main.tf
+++ vpc-setup/main.tf
@@ -7,3 +7,16 @@ resource "aws_vpc" "login-vpc" {
     Name = "login-vpc"
   }
 }
+
+
+#Subnet for frontend
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.login-vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "login-frontend-subnet"
+  }
+}
