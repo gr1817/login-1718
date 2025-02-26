fb0d4a8b2e9b2e242f7f857cae423a7fcc0fd898
gr1817 <gopirajuyadala2@gmail.com>
2025-02-27 01:37:45 +0530

    vpc tag name updated

--- vpc-setup/main.tf
+++ vpc-setup/main.tf
@@ -1,9 +1,9 @@
 #vpc
-resource "aws_vpc" "login_vpc" {
+resource "aws_vpc" "login-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "login_vpc"
+    Name = "login-vpc"
   }
 }
