9da47b75d12047996976ca922e3851c4408cbe67
gr1817 <gopirajuyadala2@gmail.com>
2025-02-27 01:46:32 +0530

    update public ip on subnet

--- vpc-setup/main.tf
+++ vpc-setup/main.tf
@@ -15,6 +15,7 @@ resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.login-vpc.id
   cidr_block = "10.0.0.0/24"
   availability_zone = "us-west-2a"
+  map_public_ip_on_launch = "true"
 
   tags = {
     Name = "login-frontend-subnet"
