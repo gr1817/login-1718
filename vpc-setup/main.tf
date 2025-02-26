b6acef99b42b08b1b05eb54a8f13d56d54d84749
gr1817 <gopirajuyadala2@gmail.com>
2025-02-27 02:33:02 +0530

    nacl

--- vpc-setup/main.tf
+++ vpc-setup/main.tf
@@ -93,3 +93,30 @@ resource "aws_route_table_association" "login-private-asc" {
   subnet_id      = aws_subnet.login-db-subnet.id
   route_table_id = aws_route_table.login-private-rt.id
 }
+
+#Nacl  
+resource "aws_network_acl" "login-nacl" {
+  vpc_id = aws_vpc.login-vpc.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/00"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/00"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "login-nacl"
+  }
+}
