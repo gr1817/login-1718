84e2ba013e955dd64ab036558ba3f51cb04e53c9
gr1817 <gopirajuyadala2@gmail.com>
2025-03-03 14:29:04 +0530

    update vars

--- vpc-setup/variables.tf
+++ vpc-setup/variables.tf
@@ -1,7 +1,35 @@
+# Varibales
 variable "aws_access_key"{
  type = string
  }
  
 variable "aws_secret_key"{
  type = string
+ }
+
+# Varibale vpc cidr
+variable "vpc_cidr"{
+ type = string
+ default = "10.0.0.0/16"
+ }
+
+# Varibale vpc tenancy
+variable "vpc_tenancy"{
+ type = string
+ default = "default"
+ }
+
+# Varibale vpc tenancy
+variable "vpc_name"{
+ type = string
+ default = "login"
+ }
+
+# Varibale public subnet 
+variable "public_subent_cidrs"{
+ type = map(string)
+  default = {
+    frontend = "10.0.0.0/24"
+    backend = "10.0.1.0/24"
+  }
  }
